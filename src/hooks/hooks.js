9faa4e168b1f8383ad2e784272b707b2c8b29fc7
SamkHarks <samikh90@gmail.com>
2023-10-19 20:19:10 +0300

    add hooks for refs & scroll

---
+++ src/hooks/hooks.js
@@ -0,0 +1,29 @@
+import { createRef, useEffect, useState } from 'react';
+import { sections } from '../components/Header';
+
+export const useRefs = () => {
+    const refs = sections.reduce((acc, section) => {
+      acc[section.id] = createRef();
+      return acc;
+    }, {});
+    return refs
+}
+
+export const useScroll = () => {
+    const [isScrolledUp, setIsScrolledUp] = useState(true);
+    useEffect(() => {
+        let prevScrollPos = window.scrollY;
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+            const isScrollingUp = currentScrollPos < prevScrollPos;
+            prevScrollPos = currentScrollPos;
+            setIsScrolledUp(isScrollingUp);
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    return isScrolledUp;
+}

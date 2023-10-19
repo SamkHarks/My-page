af7f417ee652aa85994cf3d140ea16dbaeb659e3
SamkHarks <samikh90@gmail.com>
2023-10-19 20:17:10 +0300

    add component for react spinner

---
+++ src/components/ReactHeader.js
@@ -0,0 +1,19 @@
+import React, { forwardRef } from 'react';
+import logo from '../logo.svg';
+
+export const ReactHeader = forwardRef((_props,ref) => (
+    <header ref={ref} className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p className="fade-in-element">
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+));

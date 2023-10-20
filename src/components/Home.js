81b88b54663c1634e99130847e2347ad0333d4ac
SamkHarks <samikh90@gmail.com>
2023-10-20 23:46:57 +0300

    add i18 related localizations

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,31 @@ import React from 'react';
 import { useRefs } from '../hooks/hooks';
 import { Header } from './Header';
 import { Sections } from './Sections';
+import { About } from './About';
+import { useTranslation } from 'react-i18next';
 
 export const Home = () => {
     const sectionRefs = useRefs();
+    const { t, i18n } = useTranslation("translation");
+
+    const changeLanguage = (lng) => {
+      i18n.changeLanguage(lng);
+    };
     return (
         <div className="App">
             <Header sectionRefs={sectionRefs} />
+            <div style={{
+                height: 200,
+                backgroundColor: "whitesmoke"
+            }}>
+            <button type="button" onClick={() => changeLanguage('fi')}>
+                fi
+            </button>
+            <button type="button" onClick={() => changeLanguage('en')}>
+                en
+            </button>
+                <p>{t('title')}</p>
+            </div>
             <Sections sectionRefs={sectionRefs} />
         </div>
     );

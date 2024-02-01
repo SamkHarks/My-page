66b0c82c730b397631721a8fecc915f9ffa638cd
SamkHarks <samikh90@gmail.com>
2024-02-02 00:08:05 +0200

    update to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Home } from './components/Home';
 
-function App() {
-    return (
-        <Home />
-    );
-}
+const App = () => (
+    <Home />
+);
 
 export default App;

15bff46ea80a34907e4c2f30a2cffca5d0872bb7
SamkHarks <samikh90@gmail.com>
2024-02-02 09:10:53 +0200

    convert files to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // import i18n (needs to be bundled ;))
 import './i18n';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         <App />

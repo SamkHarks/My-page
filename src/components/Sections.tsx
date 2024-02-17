395fd8ec8607ea0b363388de699d4c9b09d8722d
SamkHarks <samikh90@gmail.com>
2024-02-17 17:04:53 +0200

    add translation to sections

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -7,6 +7,7 @@ import { Contact } from './Contact';
 import '../App.css';
 import { useInterSectionObserver } from '../hooks/hooks';
 import { SectionRefs } from '../hooks/types';
+import { useTranslation } from 'react-i18next';
 
 const components: Record<string,React.ComponentType> = {
     welcome: Welcome,
@@ -16,29 +17,32 @@ const components: Record<string,React.ComponentType> = {
 };
 
 type Props = {
-    section: typeof sections[number],
-    sectionRefs: SectionRefs,
+    section: typeof sections[number];
+    sectionRefs: SectionRefs;
     children: React.ReactNode;
 }
 
 const SectionWrapper = ({
     section,
     sectionRefs,
-    children
-}: Props) => (
-    <div
-        ref={sectionRefs[section.id]}
-        className={'section'}
-        style={{ backgroundColor: section.backgroundColor }}
-    >
-        <header className={'section-title'}>{section.title}</header>
-        <div style={{
-            paddingTop: 20
-        }}>
-            {children}
+    children,
+}: Props) => {
+    const { t } = useTranslation('sections');
+    return (
+        <div
+            ref={sectionRefs[section.id]}
+            className={'section'}
+            style={{ backgroundColor: section.backgroundColor }}
+        >
+            <header className={'section-title'}>{t(section.id)}</header>
+            <div style={{
+                paddingTop: 20
+            }}>
+                {children}
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export const Sections = ({ sectionRefs }: {sectionRefs: Props['sectionRefs']}) => {
     const [data, setData] = useState<Element[]>([]);

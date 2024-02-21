84d4a18b99013792642030549c50583c9b7b30c3
SamkHarks <samikh90@gmail.com>
2024-02-21 17:36:01 +0200

    update welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,9 +4,15 @@ import React from 'react';
 export const Welcome = () => {
     return (
         <div style={{
-            color: 'white'
+            color: 'white',
+            display: 'flex',
+            flexDirection: 'column'
         }}>
-            <p>Hi, my name is Sami, and I build things for web and mobile apps.</p>
+            <code>
+                <h1>Sami Härkönen</h1>
+                <h2>Developer at Veikkaus</h2>
+                <p>Hi, my name is Sami, and I build things for web and mobile apps.</p>
+            </code>
         </div>
     );
 };

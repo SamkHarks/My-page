f213d727ef473269f530afd5bc49d5b37af35cd3
SamkHarks <samikh90@gmail.com>
2024-02-21 18:59:15 +0200

    remove code element

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -8,11 +8,9 @@ export const Welcome = () => {
             display: 'flex',
             flexDirection: 'column'
         }}>
-            <code>
-                <h1>Sami Härkönen</h1>
-                <h2>Developer at Veikkaus</h2>
-                <p>Hi, my name is Sami, and I build things for web and mobile apps.</p>
-            </code>
+            <h1 style={{fontSize: 60}}>Sami Härkönen</h1>
+            <h2>Developer at Veikkaus</h2>
+            <p>Hi, my name is Sami, and I build things for web and mobile apps.</p>
         </div>
     );
 };

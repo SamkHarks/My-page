f59951f01f1dedaf08e679df6e9cec376fb04e7b
SamkHarks <samikh90@gmail.com>
2024-02-28 09:39:50 +0200

    refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Home } from './components/Home';
 import './App.css';
+import { useRefs } from './hooks/hooks';
+import { Header } from './components/Header';
+import { Sections } from './components/Sections';
 
-const App = () => (
-    <Home />
-);
+const App = () => {
+    const sectionRefs = useRefs();
+    return (
+        <div className="App">
+            <Header sectionRefs={sectionRefs} />
+            <Sections sectionRefs={sectionRefs} />
+        </div>
+    );
+}
+
 
 export default App;

369e6be87fe8ad175011f2542dd382bd234a52ed
SamkHarks <samikh90@gmail.com>
2024-02-29 01:25:46 +0200

    update styles

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import styles from './Contact.module.css';
 
 export const Contact = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <p>Email: samikh90@gmail.com</p>
         </div>
     );

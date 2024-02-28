9824c4fcde47aea05c4ff50eb4339b13380af3dc
SamkHarks <samikh90@gmail.com>
2024-02-28 16:41:23 +0200

    update picture and text styles & add container styles

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -7,16 +7,16 @@ import styles from './About.module.css';
 export const About = () => {
     const { t } = useTranslation('about');
     return (
-        <div>
+        <div className={styles.container}>
             <img
                 className={styles.profile_image}
                 src={me}
                 alt='picture of me'
             />
-            <p>
+            <p className={styles.text}>
                 {t('introduction')}
             </p>
-            <p className={styles.margin_top}>
+            <p className={`${styles.text} ${styles.margin_top}`}>
                 {t('main')}
             </p>
         </div>

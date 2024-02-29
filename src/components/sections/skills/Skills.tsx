3650912aeda9097433a5750043c3555e4a667c33
SamkHarks <samikh90@gmail.com>
2024-02-29 13:53:40 +0200

    update skills

--- src/components/sections/skills/Skills.tsx
+++ src/components/sections/skills/Skills.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import styles from './Skills.module.css';
-import { useFetchSkills } from './hooks';
+import { useFetchData } from '../../../hooks/hooks';
+import { SkillsResponse } from './types';
+import { DataProps, ServiceData } from '../../serviceData/ServiceData';
 
 
 
 export const Skills = () => {
-    const service = useFetchSkills();
-    switch (service.state) {
-        case 'IDLE':
-        case 'LOADING':
-            return (
-                <div>
-                    <p>loading</p>
-                </div>
-            );
-        case "SUCCESS":
-            return (
-                <div className={styles.container}>
-                    <p>I have done this and that, and work over 2 years as a mobile developer at Veikkaus</p>
-                </div>
-            );
-        case 'FAILURE':
-            return (
-                <div>
-                    <p>Oops something went wrong</p>
-                </div>
-            );
-        default:
-            return null;
-    }
+    const service = useFetchData<SkillsResponse>('skills.json');
+    return <ServiceData service={service} Renderer={Renderer}/>;
+};
+
+const Renderer = (props: DataProps<SkillsResponse> ) => {
+    const { skills } = props.data;
+    return (
+        <div className={styles.container}>
+            <p>I have done this and that, and work over 2 years as a mobile developer at Veikkaus</p>
+            <div style={{ columnGap: 10, display: 'flex' }}>
+                {
+                    skills.map((item) => {
+                        return (
+                            <div key={item.category}>
+                                <p>{`${item.category}: `}</p>
+                                {
+                                    item.items.map((skill) => {
+                                        return (
+                                            <div key={skill}>
+                                                <p>{skill}</p>
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </div>
+    );
 };

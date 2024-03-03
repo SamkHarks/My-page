0f57a51a8e964c780586f36717bc836b67152feb
SamkHarks <samikh90@gmail.com>
2024-03-03 05:02:05 +0200

    add getter for spinner sizes

---
+++ src/components/spinner/utils.ts
@@ -0,0 +1,15 @@
+import { getStyle } from "../../utils/utils";
+
+const SMALL = 75;
+const MEDIUM = 100;
+const LARGE = 125;
+
+export const getSpinnerSize = (size: 'small' | 'medium' | 'large') => {
+    if (size === 'small') {
+        return { size: SMALL, strokeWidth: 1, fontSize: getStyle("--font-size-XS") };
+    } else if (size === 'medium') {
+        return { size: MEDIUM, strokeWidth: 1, fontSize: getStyle("--font-size-M") };
+    } else {
+        return { size: LARGE, strokeWidth: 2, fontSize: getStyle("--font-size-L") };
+    }
+};

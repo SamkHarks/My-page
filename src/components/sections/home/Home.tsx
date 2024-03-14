2bf85930e6467fab8019474fbce8bedf85d0fe1f
SamkHarks <samikh90@gmail.com>
2024-03-14 19:47:37 +0200

    update home section

--- src/components/sections/home/Home.tsx
+++ src/components/sections/home/Home.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import styles from "./Home.module.css";
 
 export const Home = () => {
+  const { t } = useTranslation("home");
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Sami Härkönen</h1>
-      <h2 className={styles.sub_title}>Developer at Veikkaus</h2>
-      <p className={styles.text}>
-        Passionate About Problem Solving and Building High-Performance
-        Applications for Mobile and Web.
-      </p>
+      <h2 className={styles.sub_title}>{`${t("education")} & ${t("work")}`}</h2>
+      <p className={styles.text}>{t("shortIntro")}</p>
+      <p className={styles.intro}>{t("intro")}</p>
+      <div className={styles.button_container}>
+        <button className={styles.button}>{t("resume")}</button>
+      </div>
     </div>
   );
 };

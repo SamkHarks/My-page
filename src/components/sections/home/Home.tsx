5672065eafe8c29b75945a79d97c98f58e4c91b7
SamkHarks <samikh90@gmail.com>
2024-03-14 23:09:25 +0200

    add fi and en cvs

--- src/components/sections/home/Home.tsx
+++ src/components/sections/home/Home.tsx
@@ -3,7 +3,15 @@ import { useTranslation } from "react-i18next";
 import styles from "./Home.module.css";
 
 export const Home = () => {
-  const { t } = useTranslation("home");
+  const { t, i18n } = useTranslation("home");
+
+  let link = "/cv/cv-sami-en.pdf";
+  let download = "cv-sami-härkönen-en.pdf";
+  if (i18n.language === "fi") {
+    link = "/cv/cv-sami-fi.pdf";
+    download = "cv-sami-härkönen-fi.pdf";
+  }
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Sami Härkönen</h1>
@@ -11,11 +19,7 @@ export const Home = () => {
       <p className={styles.text}>{t("shortIntro")}</p>
       <p className={styles.intro}>{t("intro")}</p>
       <div className={styles.button_container}>
-        <a
-          className={styles.button}
-          href="/cv/cv-sami-en.pdf"
-          download="cv-sami-en.pdf"
-        >
+        <a className={styles.button} href={link} download={download}>
           {t("resume")}
         </a>
       </div>

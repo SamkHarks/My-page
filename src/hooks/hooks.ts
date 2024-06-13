ba36afba1caee4a3351cbb45109397536c46ae9d
SamkHarks <samikh90@gmail.com>
2024-06-13 20:43:18 +0300

    update intesectionObserver

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -32,49 +32,48 @@ export const useScroll = () => {
 
 export const useInterSectionObserver = (data: Element[]) => {
   React.useEffect(() => {
-    if (data.length > 0) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            const target = entry.target;
-            if (target instanceof HTMLElement) {
-              if (entry.isIntersecting) {
-                // Observe items only once
-                target.addEventListener(
-                  "transitionend",
-                  (event: TransitionEvent) => {
-                    if (
-                      event.propertyName === "opacity" ||
-                      event.propertyName === "transform"
-                    ) {
-                      target.style.willChange = "";
-                      observer.unobserve(target);
-                    }
-                  },
-                  { once: true },
-                );
-                target.style.willChange = "transform, opacity";
-                target.classList.add("animate");
-              }
-            }
-          });
-        },
-        {
-          rootMargin: "-5px",
-        },
-      );
-      // Observe each section element
-      Object.values(data).forEach((item) => {
-        observer.observe(item);
-      });
-      return () => {
-        // Disconnect the observer when the component unmounts
-        Object.values(data).forEach((item) => {
-          observer.unobserve(item);
-        });
-        observer.disconnect();
-      };
+    if (data.length === 0) {
+      return;
     }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const target = entry.target;
+          if (target instanceof HTMLElement) {
+            if (entry.isIntersecting) {
+              // Observe items only once
+              target.addEventListener(
+                "transitionend",
+                (event: TransitionEvent) => {
+                  if (
+                    event.propertyName === "opacity" ||
+                    event.propertyName === "transform"
+                  ) {
+                    observer.unobserve(target);
+                  }
+                },
+                { once: true },
+              );
+              target.classList.add("animate");
+            }
+          }
+        });
+      },
+      {
+        rootMargin: "-5px",
+      },
+    );
+    // Observe each section element
+    Object.values(data).forEach((item) => {
+      observer.observe(item);
+    });
+    return () => {
+      // Disconnect the observer when the component unmounts
+      Object.values(data).forEach((item) => {
+        observer.unobserve(item);
+      });
+      observer.disconnect();
+    };
   }, [data]);
 };
 

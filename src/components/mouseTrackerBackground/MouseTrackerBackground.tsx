2b9043cd118b3e2ac73b931bde31862436779865
SamkHarks <samikh90@gmail.com>
2024-09-08 19:37:26 +0300

    add mouse tracker component

---
+++ src/components/mouseTrackerBackground/MouseTrackerBackground.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./mouseTrackerBackground.module.css";
+
+const GRADIENT_SIZE = 250;
+
+export const MouseTrackerBackground = ({ children }: React.PropsWithChildren) => {
+  const [mouse, setMouse] = React.useState({ x: 0, y: 0 });
+
+  function onMouseMove(event: React.MouseEvent<HTMLDivElement>) {
+    const { currentTarget, clientX, clientY } = event;
+    const { left, top } = currentTarget.getBoundingClientRect();
+    setMouse({ x: clientX - left, y: clientY - top });
+  }
+  // Generate the mask image style
+  const maskImage = `radial-gradient(${GRADIENT_SIZE}px at ${mouse.x}px ${mouse.y}px, rgba(255,255,255,0.7), transparent)`;
+  const style = {
+    backgroundImage: maskImage,
+    WebkitMaskImage: maskImage, // For Safari compatibility
+  };
+
+  return (
+    <div onMouseMove={onMouseMove} className={styles.container}>
+      <div className={styles.container_inner}>
+        <div className={styles.gradient} style={style} />
+        <div className={styles.overlay} style={style} />
+      </div>
+      {children}
+    </div>
+  );
+};

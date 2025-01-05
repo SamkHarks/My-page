181ceaf131bf021a3845d3ed6efd85be3cb3966b
SamkHarks <samikh90@gmail.com>
2025-01-05 23:39:55 +0200

    add assert

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,3 +2,9 @@ const root = document.documentElement;
 
 export const getStyle = (variable: string) =>
   getComputedStyle(root).getPropertyValue(variable);
+
+export const assert = (condition: boolean, message: string) => {
+  if (!condition) {
+    console.error(message);
+  }
+};

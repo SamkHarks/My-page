5864bbd8bcfb369acc0a15baf49906a3d5ec1165
SamkHarks <samikh90@gmail.com>
2025-01-07 17:53:43 +0200

    separate webgl creation and event listeners by using two different useEffects

--- src/components/animation/Animation.tsx
+++ src/components/animation/Animation.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import styles from "./Animation.module.css";
 import {
-  COLUMNS,
-  getDrawType,
   getVertices,
   getVerticesCount,
   setCanvasDimensions,
-  getRandomColorVertices,
   updateBuffers,
   updateDynamicMode,
-  setupUniforms,
-  setupBuffers,
-  createShader,
-  createProgram,
-  getFragmentShaderSource
 } from "./utils";
 import { Props } from "./types";
-import { vertexShaderSource } from "./shaders";
+import { useWebGLContext } from "./hooks";
 
 const Animation = (props: Props) => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
@@ -28,80 +20,40 @@ const Animation = (props: Props) => {
   const program = React.useRef<WebGLProgram | null>(null);
   const vBuffer = React.useRef<WebGLBuffer | null>(null);
   const cBuffer = React.useRef<WebGLBuffer | null>(null);
+  const glRef = React.useRef<WebGL2RenderingContext | null>(null);
+  const uniformsRef = React.useRef<Record<string, WebGLUniformLocation | null>>({});
   const animate = React.useRef<boolean>(false);
-  const isDeleted = program.current === null || vShader.current === null || fShader.current === null || vBuffer.current === null || cBuffer.current === null;
-  // Create WebGL context
+  //const isDeleted = program.current === null || vShader.current === null || fShader.current === null || vBuffer.current === null || cBuffer.current === null;
+  const animationStartTime = React.useRef<DOMHighResTimeStamp | null>(null);
+  const verticesCount = React.useRef<number>(0);
+  const webGLContext = {
+    canvasRef,
+    glRef,
+    vShader,
+    fShader,
+    program,
+    vBuffer,
+    cBuffer,
+    isDynamicMode,
+    uniformsRef,
+    animate,
+    animationStartTime,
+    verticesCount,
+  };
+
+  // Create the WebGL context
+  const isDeleted = useWebGLContext(props, webGLContext);
+
   React.useEffect(() => {
-    if (!canvasRef.current) return;
+    if (!canvasRef.current || !glRef.current || !program.current) return;
     const canvas = canvasRef.current;
-    const gl = canvas.getContext("webgl2", { alpha: true, antialias: props.type === 'shockwave' });
-    if (!gl) {
-      console.error("WebGL not supported");
-      return;
-    }
-    gl.lineWidth(1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    const gl = glRef.current;
+    const { u_animate, u_dynamic, u_width, u_resolution } = uniformsRef.current;
 
-    // Create shaders
-    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, getFragmentShaderSource(props.type));
-    if (vertexShader === null || fragmentShader === null) return;
-    vShader.current = vertexShader;
-    fShader.current = fragmentShader;
 
-    // Create program
-    const shaderProgram = createProgram(gl, vertexShader, fragmentShader);
-    if (shaderProgram === null) return;
-    program.current = shaderProgram;
-
-    // Setup buffers
-    const vertices = getVertices(props.type, props.numVertices);
-    const colors = getRandomColorVertices(vertices.length / 3 * 4);
-    const { vertexBuffer, colorBuffer } = setupBuffers(gl,shaderProgram, vertices, colors);
-    vBuffer.current = vertexBuffer;
-    cBuffer.current = colorBuffer;
-
-    const uniforms = setupUniforms(gl, shaderProgram, props, isDynamicMode.current);
-    const { u_time, u_animate, u_resolution, u_width, u_dynamic } = uniforms;
-
-    // Draw the scene
-    const isCircle = props.type === 'circle';
-    const drawType = getDrawType(props.type, gl);
-    let animationFrameId: number;
-    let animationStartTime: DOMHighResTimeStamp | null = null;
-    let elapsedTime = 0;
-    let verticesCount = getVerticesCount(props.type, vertices);
-    const offsetTime = 0.8;
-    const render = (time: number) => {
-      if (isDeleted) return; // Program has been deleted
-      gl.clear(gl.COLOR_BUFFER_BIT);
-      gl.useProgram(shaderProgram);
-      // Calculate the elapsed time based on the animation start time
-      const startNewTimer = (isCircle && (isDynamicMode.current || animate.current));
-      if (startNewTimer && animationStartTime !== null) {
-        elapsedTime = (time - animationStartTime) * 0.001;
-      }
-      // Pass the calculated time to the shader
-      gl.uniform1f(u_time, startNewTimer ? elapsedTime + offsetTime  : time * 0.001);
-      if (isCircle) {
-        gl.drawArrays(drawType, 0, verticesCount);
-      } else if (props.type === 'zigzag') {
-        for (let j = 0; j < COLUMNS; j++) {
-          const start = j * verticesCount;  // Starting index for this column
-          gl.drawArrays(drawType, start, verticesCount);  // Draw the zigzag column
-        }
-      } else {
-        gl.drawArrays(drawType, 0, verticesCount);
-        gl.drawArrays(drawType, verticesCount, verticesCount);
-      }
-      animationFrameId = requestAnimationFrame(render);
-    };
-    requestAnimationFrame(render);
-
-    // Control border mode in resize event
     let newNumVertices = currentNumVertices.current;
     const handleResize = () => {
-      gl.useProgram(shaderProgram);
+      gl.useProgram(program.current);
       gl.uniform1f(u_width, window.innerWidth);
 
       const isChanged = setCanvasDimensions(canvas, props.type);
@@ -125,24 +77,23 @@ const Animation = (props: Props) => {
       }
 
       if (newNumVertices === currentNumVertices.current) return; // No change in vertices count
-      if (vBuffer.current === null || cBuffer.current === null) return; // Buffers not created
+      if (vBuffer.current === null || cBuffer.current === null || program.current === null) return; // Buffers not created
       const newVertices = getVertices(props.type, newNumVertices);
-      updateBuffers(newVertices, gl, vBuffer.current, cBuffer.current, shaderProgram);
-      verticesCount = getVerticesCount(props.type, newVertices); // vertices count represenst the number of data points to draw
+      updateBuffers(newVertices, gl, vBuffer.current, cBuffer.current, program.current);
+      verticesCount.current = getVerticesCount(props.type, newVertices); // vertices count represenst the number of data points to draw
       currentNumVertices.current = newNumVertices;
     };
 
     const setAnimation = (value: boolean) => {
       if (animate.current === value) return; // Already in the same state
-      gl.useProgram(shaderProgram);
+      gl.useProgram(program.current);
       gl.uniform1i(u_animate, value ? 1 : 0);
       animate.current = value;
-      animationStartTime = value ? performance.now() : null;
+      animationStartTime.current = value ? performance.now() : null;
     };
-    // Handle scroll events
+    const isScrollendSupported = 'onscrollend' in window;
     let isScrolling = false;
     let timeout: NodeJS.Timeout;
-    const isScrollendSupported = 'onscrollend' in window;
     const handleScroll = () => {
       setAnimation(true);
       isScrolling = true;
@@ -160,7 +111,7 @@ const Animation = (props: Props) => {
 
     const handleMouseEnter = () => {
       if (isDynamicMode.current || isSmallScreen.current) return; // Already set or not allowed
-      gl.useProgram(shaderProgram);
+      gl.useProgram(program.current);
       gl.uniform1i(u_dynamic, 1);
       isDynamicMode.current = true;
       setAnimation(true);
@@ -168,42 +119,22 @@ const Animation = (props: Props) => {
 
     const handleMouseLeave = () => {
       if (!isDynamicMode.current || isSmallScreen.current) return; // Already set or not allowed
-      gl.useProgram(shaderProgram);
+      gl.useProgram(program.current);
       gl.uniform1i(u_dynamic, 0);
       isDynamicMode.current = false;
       setAnimation(isScrolling);
     };
 
     window.addEventListener('resize', handleResize);
-    if (isCircle) {
+    if (props.type === 'circle') {
       document.getElementById('about')?.addEventListener('mouseenter', handleMouseEnter);
       document.getElementById('about')?.addEventListener('mouseleave', handleMouseLeave);
       window.addEventListener('scroll', handleScroll);
       window.addEventListener('scrollend', handleScrollEnd);
     }
-
     return () => {
-      if (!gl) return;
-      // Cleanup
-      cancelAnimationFrame(animationFrameId);
-      // Unset the current program before deleting it
-      gl.useProgram(null);
-
-      gl.clear(gl.COLOR_BUFFER_BIT);
-      if (vShader.current) gl.deleteShader(vShader.current);
-      if (fShader.current) gl.deleteShader(fShader.current);
-      if (program.current) gl.deleteProgram(program.current);
-      if (vBuffer.current) gl.deleteBuffer(vBuffer.current);
-      if (cBuffer.current) gl.deleteBuffer(cBuffer.current);
-
-      vShader.current = null;
-      fShader.current = null;
-      program.current = null;
-      vBuffer.current = null;
-      cBuffer.current = null;
-
       window.removeEventListener('resize', handleResize);
-      if (isCircle) {
+      if (props.type === 'circle') {
         window.removeEventListener('scroll', handleScroll);
         window.removeEventListener('scrollend', handleScrollEnd);
         document.getElementById('about')?.removeEventListener('mouseenter', handleMouseEnter);
@@ -211,10 +142,7 @@ const Animation = (props: Props) => {
         clearTimeout(timeout);
       }
     };
-
-  }, [
-    isDeleted
-  ]);
+  }, [isDeleted]);
 
   return (
     <canvas

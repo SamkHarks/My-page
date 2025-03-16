835fcaf12487ec30f03662cb89e2626c15df28f2
SamkHarks <samikh90@gmail.com>
2025-03-16 21:23:30 +0200

    lazy load language resources

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,54 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import resourcesToBackend from "i18next-resources-to-backend";
 
-// Importing translation files
-import sectionsEn from "src/locales/en/sections.json";
-import sectionsFi from "src/locales/fi/sections.json";
-import aboutEn from "src/locales/en/about.json";
-import aboutFi from "src/locales/fi/about.json";
-import skillsEn from "src/locales/en/skills.json";
-import skillsFi from "src/locales/fi/skills.json";
-import commonEn from "src/locales/en/common.json";
-import commonFi from "src/locales/fi/common.json";
-import homeEn from "src/locales/en/home.json";
-import homeFi from "src/locales/fi/home.json";
+// Define the supported languages
+type Language = "fi" | "en";
 
-export type Languages = "en" | "fi";
-type Resource = Record<Languages, {
-  sections: typeof sectionsEn;
-  about: typeof aboutEn;
-  skills: typeof skillsEn;
-  common: typeof commonEn;
-  home: typeof homeEn;
-}>;
-
-const resources: Resource = {
-  en: {
-    sections: sectionsEn,
-    about: aboutEn,
-    skills: skillsEn,
-    common: commonEn,
-    home: homeEn,
-  },
-  fi: {
-    sections: sectionsFi,
-    about: aboutFi,
-    skills: skillsFi,
-    common: commonFi,
-    home: homeFi,
-  },
-};
+// Define the supported namespaces
+type Namespace = "common" | "home" | "about" | "skills" | "sections";
 
 i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
+  .use(resourcesToBackend((language: Language, namespace: Namespace) => import(`./locales/${language}/${namespace}.json`))) // Lazy-load translations
+  .use(initReactI18next) // Initialize react-i18next
   .init({
-    fallbackLng: "en",
-    resources,
-    debug: process.env.NODE_ENV === "development",
+    lng: "en", // Default language
+    debug: process.env.NODE_ENV === "development", // Enable debug mode in development
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false, // React already escapes values by default
     },
   });
 

accd3b826d6fe6ac8b14c23c83b40687603370da
SamkHarks <samikh90@gmail.com>
2025-03-22 17:05:22 +0200

    wrap app with errorboundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import { createRoot } from "react-dom/client";
 import "src/global/global.css";
 import App from "src/components/app/App";
-import { Modal } from "src/components/modal/Modal";
 
 // import i18n (needs to be bundled ;))
 import "src/i18n";
 import { StrictMode } from "react";
+import { ErrorBoundary } from "src/components/boundaries/errorBoundary/ErrorBoundary";
 
 const root = createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <StrictMode>
-    <App />
-    <Modal />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 );

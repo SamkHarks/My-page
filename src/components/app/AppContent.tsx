eddb194d9f40c38e843f84b313dec22c19f99c74
SamkHarks <samikh90@gmail.com>
2025-03-22 17:06:49 +0200

    wrap sections with Boundaries

--- src/components/app/AppContent.tsx
+++ src/components/app/AppContent.tsx
@@ -1,10 +1,11 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { DataProps } from "src/components/serviceData/ServiceData";
 import { SectionResponse } from "src/components/app/types";
 import { useRefs } from "src/hooks/hooks";
 import { Header } from "src/components/header/Header";
 import { Loader } from "src/components/loader/Loader";
 import { MouseTrackerBackground } from "src/components/mouseTrackerBackground/MouseTrackerBackground";
+import { Boundaries } from "src/components/boundaries/Boundaries";
 
 const Sections = lazy(() => import("../sections/Sections"));
 
@@ -14,11 +15,13 @@ export const AppContent = ({ data }: DataProps<SectionResponse>): React.JSX.Elem
   return (
     <div>
       <Header sectionRefs={sectionRefs} sections={sections} />
-      <Suspense fallback={<Loader size={"medium"} />}>
+      <Boundaries
+        LoadingFallback={<Loader size={"medium"} />}
+      >
         <MouseTrackerBackground>
           <Sections sectionRefs={sectionRefs} sections={sections} />
         </MouseTrackerBackground>
-      </Suspense>
+      </Boundaries>
     </div>
   );
 };

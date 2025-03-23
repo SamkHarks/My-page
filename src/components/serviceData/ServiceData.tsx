949557fce4f4f01ee327821394e344a9c961cc2d
SamkHarks <samikh90@gmail.com>
2025-03-23 09:12:58 +0200

    add refetch to handled errors

--- src/components/serviceData/ServiceData.tsx
+++ src/components/serviceData/ServiceData.tsx
@@ -1,5 +1,6 @@
 import { Service } from "src/hooks/types";
 import { Loader } from "src/components/loader/Loader";
+import { HandledError } from "src/components/boundaries/errorBoundary/HandledError";
 
 export type DataProps<T> = {
   data: T;
@@ -8,14 +9,15 @@ export type DataProps<T> = {
 type Props<T> = {
   service: Service<T>;
   Renderer: React.ComponentType<DataProps<T>>;
+  refetch?: () => void;
   LoadingFallback?: React.JSX.Element;
   spinnerSize?: "small" | "medium" | "large";
 };
 
-//TODO add components for failure and idle/loading
 export const ServiceData = <T,>({
   service,
   Renderer,
+  refetch,
   LoadingFallback,
   spinnerSize = "medium",
 }: Props<T>): React.JSX.Element | null => {
@@ -25,12 +27,11 @@ export const ServiceData = <T,>({
       return LoadingFallback ?? <Loader size={spinnerSize} />;
     case "SUCCESS":
       return <Renderer data={service.data} />;
-    case "FAILURE":
-      return (
-        <div>
-          <p>Oops something went wrong</p>
-        </div>
-      );
+    case "FAILURE": 
+      if (service.error instanceof HandledError && refetch) {
+        service.error.reset = refetch;
+      }
+      throw service.error;// Let ErrorBoundary catch this
     default:
       return null;
   }

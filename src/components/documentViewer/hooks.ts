a7f61eded145421f53290e3d3dbae9c78ebb62f1
SamkHarks <samikh90@gmail.com>
2025-03-23 12:01:23 +0200

    add hook for checking links

---
+++ src/components/documentViewer/hooks.ts
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useMemo } from "react";
+import { useAcyncFunction } from "src/hooks/hooks";
+import { Service } from "src/hooks/types";
+import { HandledError } from "src/components/boundaries/errorBoundary/HandledError";
+import { handleNetworkError } from "src/utils/utils";
+
+
+
+export const useCheckLink = (link: string): {
+  service: Service<string>;
+  refetch: () => Promise<void>;
+} => {
+  const checkLink = useCallback(async () => {
+    const response = await fetch(link, { method: 'HEAD' });
+    if (response.ok) {
+      return link;
+    }
+    const errorArgs = handleNetworkError(response.status);
+    throw new HandledError(errorArgs.key, errorArgs.args);
+  }, [link]);
+
+  const [service, callService] = useAcyncFunction<string>(checkLink);
+  
+  useEffect(() => {
+    callService();
+  }, [callService]);
+
+  return useMemo(() => ({service, refetch: callService}), [service, callService]);
+}

0a5a10cc43886fc8719db30cbfc02078fe0b64ba
SamkHarks <samikh90@gmail.com>
2025-04-03 11:37:33 +0300

    add TextArea to Email and handle manually validation from handleSubmit

--- src/components/email/Email.tsx
+++ src/components/email/Email.tsx
@@ -1,17 +1,29 @@
 import * as styles from 'src/components/email/Email.module.css';
 import { InputField } from 'src/components/email/InputField';
+import { TextArea } from 'src/components/email/TextArea';
 
 
 export const Email = (): React.JSX.Element => {
-
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    const form = event.currentTarget;
+    if(!form.checkValidity()) {
+      return; // Prevent submission if the form is invalid
+    }
+    // TODO: Handle form submission here
+  }
   return (
     <div className={styles.container}>
-      <form className={styles.form_container}>
+      <form
+        className={styles.form_container}
+        noValidate={true}
+        onSubmit={handleSubmit}
+      >
         <InputField
           label={'name'}
           placeholder={'Your Name'}
           type={'text'}
-          required={true}        
+          required={true}
         />
         <InputField
           label={'email'}
@@ -20,16 +32,12 @@ export const Email = (): React.JSX.Element => {
           required={true}
           spellCheck={false}
         />
-        <div className={styles.input_container}>
-          <label className={styles.label} htmlFor={"message"}>Message</label>
-          <textarea
-            className={styles.input}
-            id={"message"}
-            placeholder={"Your message here..."} 
-            rows={5}
-            required={true}
-          />
-        </div>
+        <TextArea
+          label={'message'}
+          placeholder={'Your message...'}
+          required={true}
+          rows={10}
+        />
         <button type={'submit'}>Send</button>
       </form>
     </div>

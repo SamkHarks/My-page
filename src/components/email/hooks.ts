c9cc1f38f0a536f65bbac6e8d88fbc154cf485bf
SamkHarks <samikh90@gmail.com>
2025-05-02 23:37:08 +0300

    add hook for email service

---
+++ src/components/email/hooks.ts
@@ -0,0 +1,46 @@
+import { useEffect, useMemo } from 'react';
+import { useConfiguration, useService } from 'src/hooks/hooks';
+import { useModalStore } from 'src/stores/useModalStore';
+import { useNotificationStore } from 'src/stores/useNotificationStore';
+import { Service } from 'src/hooks/types';
+import { EmailResponse, FormData } from 'src/components/email/types';
+import { useTranslation } from 'react-i18next';
+
+
+
+export const useEmailService = (formData: FormData | null): {
+  service: Service<EmailResponse>;
+  callService: () => Promise<void>;
+  clearService: () => void;
+} => {
+  const { t } = useTranslation('contact');
+  const addNotification = useNotificationStore((state) => state.addNotification);
+  const setLoading = useModalStore(state => state.setLoading);
+  const {baseUrls, paths} = useConfiguration();
+
+  const urlOptions = { baseUrl: baseUrls.baseUrl, path: paths.email.contact + 'dasda' };
+  const serviceOptions = useMemo(() => ({ immediate: false }), []);
+  const requestOptions = useMemo(() => ({
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    ...(formData && { body: formData }),
+  } as const), [formData]);
+  
+  const email = useService<{message: string}>({
+    urlOptions,
+    requestOptions,
+    serviceOptions
+  });
+
+  useEffect(() => {
+    setLoading(email.service.state === 'LOADING');
+    if (email.service.state === 'SUCCESS') {
+      addNotification(t('form.success.message'), 'success', { dismissAfter: 5000 });
+    } else if (email.service.state === 'FAILURE') {
+      addNotification(t('form.failure.message'), 'error', { onDissmiss: () => email.clearService(), dismissAfter: 8000});
+    }
+
+  }, [email, setLoading, addNotification, t]);
+
+  return email;
+}

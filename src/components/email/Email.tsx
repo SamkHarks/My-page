fa47959c0a012f78da972e71b45263472ac8162a
SamkHarks <samikh90@gmail.com>
2025-05-02 23:40:18 +0300

    handle form submission

--- src/components/email/Email.tsx
+++ src/components/email/Email.tsx
@@ -2,19 +2,39 @@ import { useTranslation } from 'react-i18next';
 import * as styles from 'src/components/email/Email.module.css';
 import { InputField } from 'src/components/email/InputField';
 import { TextArea } from 'src/components/email/TextArea';
+import { useEffect, useState } from 'react';
+import { Spinner } from 'src/components/spinner/Spinner';
+import { useEmailService } from 'src/components/email/hooks';
+import { ContactForm } from 'src/components/email/types';
+import { Title } from 'src/components/email/Title';
 
 
 export const Email = (): React.JSX.Element => {
-  const {t} = useTranslation('contact'); // Use the translation hook to get the translation function
+  const {t} = useTranslation('contact');
+  const [formData, setFormData] = useState<{name: string, email: string, message: string} | null>(null);
+  const {service, callService} = useEmailService(formData);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // Prevent the default form submission behavior
+  useEffect(() => {
+    if (formData) {
+      callService();
+    }
+  },[formData, callService]);
+ 
+  const handleSubmit = (event: React.FormEvent<ContactForm>) => {
+    event.preventDefault();
     const form = event.currentTarget;
     if(!form.checkValidity()) {
       return; // Prevent submission if the form is invalid
     }
-    // TODO: Handle form submission here
+
+    const data = {
+      name: form.elements.name.value,
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    setFormData(data);
   }
+  const isSuccess = service.state === 'SUCCESS';
   return (
     <div className={styles.container}>
       <form
@@ -22,21 +42,14 @@ export const Email = (): React.JSX.Element => {
         noValidate={true}
         onSubmit={handleSubmit}
       >
-        <p className={styles.header}>
-          {t('form.intro.line1')}<br />
-          {t('form.intro.line2_pre')}
-          <span className={styles.line}>{t('form.intro.nan_equals')}</span>
-          {t('form.intro.line2_mid')}
-          <span className={styles.line}>{t('form.intro.false_text')}</span>
-          <br />
-          {t('form.intro.line3')}
-        </p>
+        <Title state={service.state}/>
         <InputField
           label={t('form.name')}
           id={'name'}
           placeholder={t('form.placeholder.name')}
           type={'text'}
           required={true}
+          disabled={isSuccess}
         />
         <InputField
           label={t('form.email')}
@@ -45,20 +58,26 @@ export const Email = (): React.JSX.Element => {
           type={'email'}
           required={true}
           spellCheck={false}
+          disabled={isSuccess}
         />
         <TextArea
           label={t('form.message')}
           id={'message'}
           placeholder={t('form.placeholder.message')}
           required={true}
+          disabled={isSuccess}
           rows={10}
         />
-        <button
+        {service.state === 'LOADING'
+        ? <div className={styles.spinner}><Spinner size={40} /></div>
+        : <button
           className={styles.submit_button}
           type={'submit'}
+          disabled={isSuccess || service.state === 'FAILURE'}
         >
           {t('form.submit')}
         </button>
+        }
       </form>
     </div>
   )
